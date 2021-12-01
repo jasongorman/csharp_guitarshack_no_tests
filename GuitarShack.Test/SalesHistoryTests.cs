5a83406a7e368fe7e2f7d484b7f81ee551e16a02
Jason Gorman <jason.gorman@codemanship.com>
2021-12-01 10:04:24 +0000

    Fixed regional variation in date-time format for parsing in test

--- GuitarShack.Test/SalesHistoryTests.cs
+++ GuitarShack.Test/SalesHistoryTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using Xunit;
 
 namespace GuitarShack.Test
@@ -8,8 +9,9 @@ namespace GuitarShack.Test
         [Fact]
         public void FetchesTotalSalesForProductWithinDateRange()
         {
+            CultureInfo provider = CultureInfo.InvariantCulture;
             ISalesHistory salesHistory = new ProductSalesHistory(new WebService<Sales>($"https://gjtvhjg8e9.execute-api.us-east-2.amazonaws.com/default/sales"));
-            Assert.Equal(16, salesHistory.Total(811, DateTime.Parse("17/7/2020"), DateTime.Parse("27/7/2020")));
+            Assert.Equal(16, salesHistory.Total(811, DateTime.ParseExact("7/17/2020", "M/d/yyyy", provider), DateTime.ParseExact("7/27/2020", "M/d/yyyy", provider)));
         }
     }
 }
