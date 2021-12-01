98260f8ba09853e3d07762b5c24f81544220c641
Jason Gorman <jason.gorman@codemanship.com>
2021-12-01 10:45:52 +0000

    Added StockMonitor web service as placeholder

---
+++ GuitarShack.Web/Controllers/StockMonitorController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GuitarShack.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StockMonitorController: ControllerBase
+    {
+        private readonly ILogger<StockMonitorController> _logger;
+
+        public StockMonitorController(ILogger<StockMonitorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public StockCheckResult Post(SaleEvent sale)
+        {
+            return new StockCheckResult() {alertSent = false, message = ""};
+        }
+    }
+
+    public class SaleEvent
+    {
+        public int productId { get; set; }
+        public int quantity { get; set; }
+    }
+
+    public class StockCheckResult
+    {
+        public bool alertSent { get; set; }
+        public string message { get; set; }
+    }
+}
